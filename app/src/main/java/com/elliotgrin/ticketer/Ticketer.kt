7d6b46030d81db95ca7dfcbf49d461ab821375c8
Roman Kuznetsov <rpkuznetsov@gmail.com>
2020-11-24 16:54:57 +0300

    Add Ticketer application

--- app/src/main/java/com/elliotgrin/ticketer/Ticketer.kt
+++ app/src/main/java/com/elliotgrin/ticketer/Ticketer.kt
@@ -1,4 +1,28 @@
 package com.elliotgrin.ticketer
 
-class Ticketer {
+import android.app.Application
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import timber.log.Timber
+
+class Ticketer : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initLogger()
+        initKoin()
+    }
+
+    private fun initLogger() {
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidContext(this@Ticketer)
+            modules()
+        }
+    }
+
 }
