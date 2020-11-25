0f3c0b03a8a7a42a0156265fb49a3700517cd45c
Roman Kuznetsov <rpkuznetsov@gmail.com>
2020-11-25 16:09:14 +0300

    Add SearchFragment UI

--- app/src/main/java/com/elliotgrin/ticketer/MainActivity.kt
+++ app/src/main/java/com/elliotgrin/ticketer/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.elliotgrin.ticketer
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.elliotgrin.ticketer.search.SearchFragment
 import org.koin.androidx.fragment.android.setupKoinFragmentFactory
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
         setupKoinFragmentFactory()
+        super.onCreate(savedInstanceState)
         openSearchFragment()
     }
 
--- app/src/main/java/com/elliotgrin/ticketer/search/SearchFragment.kt
+++ app/src/main/java/com/elliotgrin/ticketer/search/SearchFragment.kt
@@ -3,7 +3,6 @@ package com.elliotgrin.ticketer.search
 import androidx.fragment.app.Fragment
 import com.elliotgrin.ticketer.R
 
-
 class SearchFragment(private val viewModel: SearchViewModel) : Fragment(R.layout.fragment_search) {
 
 }
