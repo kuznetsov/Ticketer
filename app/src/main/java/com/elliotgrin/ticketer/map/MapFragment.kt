24467244c7114b8bcbef21929f4954dc37a083d6
Roman Kuznetsov <rpkuznetsov@gmail.com>
2020-12-03 13:27:02 +0300

    Draw geodesic polyline between markers on map

--- app/src/main/java/com/elliotgrin/ticketer/map/MapFragment.kt
+++ app/src/main/java/com/elliotgrin/ticketer/map/MapFragment.kt
@@ -2,6 +2,7 @@ package com.elliotgrin.ticketer.map
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.elliotgrin.ticketer.R
 import com.elliotgrin.ticketer.main.MainViewModel
@@ -10,10 +11,12 @@ import com.elliotgrin.ticketer.util.MapMarkerUtil
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.map_fragment.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+private const val PATTERN_GAP_LENGTH_PX = 15f
+private const val POLYLINE_WIDTH_PX = 15f
 
 class MapFragment(
     private val viewModel: MapViewModel,
@@ -45,9 +48,16 @@ class MapFragment(
     override fun onMapReady(googleMap: GoogleMap?) {
         val departure = Marker(sharedViewModel.departureCity ?: return)
         val arrival = Marker(sharedViewModel.arrivalCity ?: return)
+        setupMap(googleMap)
         setMarkers(googleMap, departure, arrival)
+        drawPlanePath(googleMap, departure, arrival)
     }
 
+    private fun setupMap(googleMap: GoogleMap?) {
+        googleMap?.uiSettings?.isRotateGesturesEnabled = false
+    }
+
+    // TODO: 02.12.2020 fix anchors and dim markers
     private fun setMarkers(googleMap: GoogleMap?, departure: Marker, arrival: Marker) {
         val bitmap1 = mapMarkerUtil.createBitmapFromMarker(departure)
         val bitmap2 = mapMarkerUtil.createBitmapFromMarker(arrival)
@@ -64,4 +74,22 @@ class MapFragment(
         }
     }
 
+    private fun moveMap(googleMap: GoogleMap, departure: Marker, arrival: Marker) {
+        // TODO: 02.12.2020 move map in center between two markers
+    }
+
+    private fun drawPlanePath(googleMap: GoogleMap?, departure: Marker, arrival: Marker) {
+        val polylineOptions = PolylineOptions().apply {
+            add(departure.location)
+            add(arrival.location)
+            geodesic(true)
+            width(POLYLINE_WIDTH_PX)
+            color(ContextCompat.getColor(requireContext(), R.color.gray_900_50))
+            pattern(listOf(Dot(), Gap(PATTERN_GAP_LENGTH_PX)))
+        }
+
+        googleMap?.addPolyline(polylineOptions)
+    }
+
 }
+
