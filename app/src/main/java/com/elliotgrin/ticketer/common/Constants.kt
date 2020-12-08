abb842c6f74464855ec3022a8a9184eb1c948625
Roman Kuznetsov <rpkuznetsov@gmail.com>
2020-12-08 12:24:18 +0300

    Decrease plane animation duration

--- app/src/main/java/com/elliotgrin/ticketer/common/Constants.kt
+++ app/src/main/java/com/elliotgrin/ticketer/common/Constants.kt
@@ -2,4 +2,4 @@ package com.elliotgrin.ticketer.common
 
 const val HOST_URL = "https://yasen.hotellook.com/"
 
-const val PLANE_ANIMATION_STEP_DURATION_MS = 1000L
+const val PLANE_ANIMATION_STEP_DURATION_MS = 500L
